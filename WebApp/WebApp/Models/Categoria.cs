a9559d42425e408c06f5d5a886ddd583cc672426
Maria Vitória <costa.nascimento@academico.ifrn.edu.br>
2022-05-23 07:52:46 -0300

    inclusão

---
+++ WebApp/WebApp/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+   
+}
