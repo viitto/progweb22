318dec0530fef0167296b28fac53edbdd484cef0
Maria Vitória Nascimento da Costa <costa.nascimento@academico.ifrn.edu.br>
2022-06-13 08:25:16 -0300

    inicio (acesso ao banco de dados)

--- WebApp/WebApp/context/EFContext.cs
+++ WebApp/WebApp/context/EFContext.cs
@@ -9,8 +9,8 @@ namespace WebApp.context
 {
     public class EFContext : DbContext
     {
-            public EFContext() : base("Asp_Net_MVC_CS") { }
-            public DbSet<Categoria> Categorias { get; set; }
-            public DbSet<Fabricante> Fabricantes { get; set; }
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
     }
- }
+}
